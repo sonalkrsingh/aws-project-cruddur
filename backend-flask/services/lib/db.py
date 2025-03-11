309488cfa1079964b3b86af60414270a3cb4e720
sonalkrsingh <sonalkrsingh05@gmail.com>
2025-03-11 18:03:53 +0530

    DynamoDb Utility Scripts-2

--- backend-flask/services/lib/db.py
+++ backend-flask/services/lib/db.py
@@ -1,8 +1,11 @@
+from dotenv import load_dotenv
 from psycopg_pool import ConnectionPool
 import os
 import re
 import sys
 from flask import current_app as app
+
+load_dotenv()  # Load environment variables from .env file
 
 class Db:
   def __init__(self):
@@ -13,9 +16,17 @@ class Db:
         if self.pool is None:
             connection_url = os.getenv("CONNECTION_URL")
             print("🔍 CONNECTION_URL:", connection_url)
+
             if not connection_url:
               raise ValueError("❌ CONNECTION_URL is not set!")
-            self.pool = ConnectionPool(connection_url, min_size=1, max_size=10)  # Limit pool size
+            
+            try:
+                print("🛠️ Attempting to create connection pool...")
+                self.pool = ConnectionPool(connection_url, min_size=1, max_size=10)
+                print("✅ Connection pool initialized successfully!")
+            except Exception as e:
+                print(f"🔥 Failed to initialize DB pool: {e}")
+                self.pool = None
 
   def template(self,*args):
     pathing = list((app.root_path,'db','sql',) + args)
@@ -41,11 +52,11 @@ class Db:
     for key, value in params.items():
       print(key, ":", value)
 
-  def print_sql(self,title,sql):
+  def print_sql(self,title,sql,params={}):
     cyan = '\033[96m'
     no_color = '\033[0m'
     print(f'{cyan} SQL STATEMENT-[{title}]------{no_color}')
-    print(sql)
+    print(sql,params)
 
   def query_commit(self, sql, params):
     try:
@@ -62,7 +73,7 @@ class Db:
 
   # when we want to return a json object
   def query_array_json(self,sql,params={}):
-    self.print_sql('array',sql)
+    self.print_sql('array',sql,params)
 
     wrapped_sql = self.query_wrap_array(sql)
     with self.pool.connection() as conn:
@@ -73,7 +84,7 @@ class Db:
   # When we want to return an array of json objects
   def query_object_json(self,sql,params={}):
 
-    self.print_sql('json',sql)
+    self.print_sql('json',sql,params)
     self.print_params(params)
     wrapped_sql = self.query_wrap_object(sql)
 
@@ -85,6 +96,14 @@ class Db:
           "{}"
         else:
           return json[0]
+  
+  def query_value(self,sql,params={}):
+     self.print_sql('value',sql,params)
+     with self.pool.connection() as conn:
+       with conn.cursor() as cur:
+         cur.execute(sql,params)
+         json = cur.fetchone()
+         return json[0]
 
   def query_wrap_object(self,template):
     sql = f"""
@@ -119,6 +138,3 @@ class Db:
         self.pool = None
 
 db = Db()
-
-db.close_pool()
-
